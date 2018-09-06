1c88a796e519bfb13c0071530f9427694d0ddcc0
VotaryTech <krishna.singh@votarytech.com>
2018-09-06 15:43:11 +0530

    my snapshot2

--- string1.c
+++ string1.c
@@ -4,6 +4,8 @@
 int main()
 {
 	char str[50]="krishna";
+	char str1[50]="singh";
 	printf("original string is %s\n",str);
 	printf("Reversed string is %s\n",strrev(str));
+	printf("concatenation of two string is %s\n,strcat(str,str1));
 }
